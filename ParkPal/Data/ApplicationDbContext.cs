d1d0ac4cdd4ced35933695ae8c3d4cfd34c6c81a
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-05 21:06:33 -0500

    add Space to DbContext
    Added Model for DB table creation via EntityFramework.

--- ParkPal/Data/ApplicationDbContext.cs
+++ ParkPal/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
 namespace ParkPal.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Space> Spaces { get; set; }
     }
 }
