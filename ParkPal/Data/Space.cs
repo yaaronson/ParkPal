fd57e0d7f046d2b2f24a2fd74725a57d3bfbba3f
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-05 21:05:08 -0500

    create Spaces model
    Created a "Space" class.
    
    EF will use this class to update the Database/Migrations.

---
+++ ParkPal/Data/Space.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkPal.Data
+{
+    public class Space
+    {
+        [Key]
+        public string SpaceId { get; set; }
+
+        [ForeignKey("User")]
+        public string HostId { get; set; }
+
+        public string? Country { get; set; }
+        public string? Province { get; set; }
+        public string? PostalCode { get; set; }
+        public decimal XCoord { get; set; }
+        public decimal YCoord { get; set; }
+
+        // Consider using an Enum for Size if you have a fixed set of size categories
+        public string? Size { get; set; }
+
+        public ApplicationUser User { get; set; } // Navigation property
+    }
+}
