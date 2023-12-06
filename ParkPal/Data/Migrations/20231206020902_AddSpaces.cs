34aadb98b921e662124fd39a3b8fe43de3233a4a
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-05 22:11:39 -0500

    create/apply db migrations
    Created Database Migrations for Spaces.
    
    SCHEMA IS TENTATIVE!

---
+++ ParkPal/Data/Migrations/20231206020902_AddSpaces.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkPal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSpaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spaces",
+                columns: table => new
+                {
+                    SpaceId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HostId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Province = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    XCoord = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    YCoord = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spaces", x => x.SpaceId);
+                    table.ForeignKey(
+                        name: "FK_Spaces_AspNetUsers_HostId",
+                        column: x => x.HostId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spaces_HostId",
+                table: "Spaces",
+                column: "HostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spaces");
+        }
+    }
+}
