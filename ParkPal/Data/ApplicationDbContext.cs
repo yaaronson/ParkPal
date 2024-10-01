7aba6a2a455070e710010fbe9b8562f1a502f00f
Jonathan Aaronson <112821604+yaaronson@users.noreply.github.com>
2024-10-01 09:56:51 -0400

    Update ApplicationDbContext.cs

--- ParkPal/Data/ApplicationDbContext.cs
+++ ParkPal/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace ParkPal.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
